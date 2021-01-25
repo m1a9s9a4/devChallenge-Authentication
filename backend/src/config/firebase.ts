7dc2e2c45d62df1e47e0db443ef19dfe3919d9ce
masafumi kato <xslash1105@gmail.com>
2021-01-25 23:02:05 +0900

    [add] firebase added

---
+++ backend/src/config/firebase.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import firebase from 'firebase';
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIRBEASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,42 @@
 import fastify from 'fastify';
+import firebase from './config/firebase';
 
 const server = fastify();
 
-server.get('/ping', async (req, res) => {
+interface IEmailPassword {
+  email: string;
+  password: string;
+}
+
+server.get('/', async (req, res) => {
+  return 'hello world';
+})
+
+server.post('/register/email', async (req, res) => {
+  const {email, password} = <IEmailPassword>req.body;
+  try {
+    return await firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        return true;
+      })
+      .catch(e => {
+        return false;
+      });
+  } catch (e) {
+    return false;
+  }
+});
+
+server.get('/register/google' ,async (req, res) => {
+
+})
+
+server.get('/login/email', async (req, res) => {
   return 'pong';
+});
+
+server.get('/login/facebook', async (req, res) => {
+
 })
 
 server.listen(8080, (err, address) => {
